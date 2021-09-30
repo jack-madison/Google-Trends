2e8a0a5b2eccb550aebaeda69e28761a5c975d44
Jack Madison <madison@sfu.ca>
2021-09-30 14:39:40 -0700

    update

---
+++ tseng_method/tseng_method_prefecture.py
@@ -0,0 +1,190 @@
+from datetime import datetime, timedelta, date, time
+import pandas as pd
+import time
+
+from pytrends.request import TrendReq
+from pytrends.exceptions import ResponseError
+
+### Taken from https://github.com/qztseng/google-trends-daily
+
+pytrend = TrendReq()
+keyword = '舌下免疫療法'
+start = '2018-12-30'
+end = '2021-01-02'
+geo='JP'
+cat=0
+gprop=''
+
+prefectures = {'Hokkaido': '01', 'Aomori': '02', 'Iwate': '03', 'Miyagi': '04', 'Akita': '05', 'Yamagata': '06',
+'Fukushima': '07', 'Ibaraki': '08', 'Tochigi': '09', 'Gunma': '10', 'Saitama': '11', 'Chiba': '12', 'Tokyo': '13',
+'Kanagawa': '14', 'Niigata': '15', 'Toyama': '16', 'Ishikawa': '17', 'Fukui': '18', 'Yamanashi': '19', 'Nagano': '20',
+'Gifu': '21', 'Shizuoka': '22', 'Aichi': '23', 'Mie': '24', 'Shiga': '25', 'Kyoto': '26', 'Osaka': '27', 'Hyogo': '28',
+'Nara': '29', 'Wakayama': '30', 'Tottori': '31', 'Shimane': '32', 'Okayama': '33', 'Hiroshima': '34', 'Yamaguchi': '35',
+'Tokushima': '36', 'Kagawa': '37', 'Ehime': '38', 'Kochi': '39', 'Fukuoka': '40', 'Saga': '41', 'Nagasaki': '42', 
+'Kumamoto': '43', 'Oita': '44', 'Miyazaki': '45', 'Kagoshima': '46', 'Okinawa': '47'}
+
+# Define the functions created by Tseng (2019)
+def _fetch_data(trendreq, kw_list, timeframe='today 3-m', cat=0, geo='', gprop='') -> pd.DataFrame:
+    
+    """Download google trends data using pytrends TrendReq and retries in case of a ResponseError."""
+    attempts, fetched = 0, False
+    while not fetched:
+        try:
+            trendreq.build_payload(kw_list=kw_list, timeframe=timeframe, cat=cat, geo=geo, gprop=gprop)
+        except ResponseError as err:
+            print(err)
+            print(f'Trying again in {60 + 5 * attempts} seconds.')
+            time.sleep(60 + 5 * attempts)
+            attempts += 1
+            if attempts > 3:
+                print('Failed after 3 attemps, abort fetching.')
+                break
+        else:
+            fetched = True
+    return trendreq.interest_over_time()
+
+def get_daily_trend(trendreq, keyword:str, start:str, end:str, cat=0, 
+                    geo='', gprop='', delta=269, overlap=100, sleep=0, 
+                    tz=0, verbose=False) ->pd.DataFrame:
+
+    """Stich and scale consecutive daily trends data between start and end date.
+    This function will first download piece-wise google trends data and then 
+    scale each piece using the overlapped period. 
+        Parameters
+        ----------
+        trendreq : TrendReq
+            a pytrends TrendReq object
+        keyword: str
+            currently only support single keyword, without bracket
+        start: str
+            starting date in string format:YYYY-MM-DD (e.g.2017-02-19)
+        end: str
+            ending date in string format:YYYY-MM-DD (e.g.2017-02-19)
+        cat, geo, gprop, sleep: 
+            same as defined in pytrends
+        delta: int
+            The length(days) of each timeframe fragment for fetching google trends data, 
+            need to be <269 in order to obtain daily data.
+        overlap: int
+            The length(days) of the overlap period used for scaling/normalization
+        tz: int
+            The timezone shift in minute relative to the UTC+0 (google trends default).
+            For example, correcting for UTC+8 is 480, and UTC-6 is -360 
+    """
+    
+    start_d = datetime.strptime(start, '%Y-%m-%d')
+    init_end_d = end_d = datetime.strptime(end, '%Y-%m-%d')
+    init_end_d.replace(hour=23, minute=59, second=59)    
+    delta = timedelta(days=delta)
+    overlap = timedelta(days=overlap)
+
+    itr_d = end_d - delta
+    overlap_start = None
+
+    df = pd.DataFrame()
+    ol = pd.DataFrame()
+    
+    while end_d > start_d:
+        tf = itr_d.strftime('%Y-%m-%d')+' '+end_d.strftime('%Y-%m-%d')
+        if verbose: print('Fetching \''+keyword+'\' for period:'+tf)
+        temp = _fetch_data(trendreq, [keyword], timeframe=tf, cat=cat, geo=geo, gprop=gprop)
+        temp.drop(columns=['isPartial'], inplace=True)
+        temp.columns.values[0] = tf
+        ol_temp = temp.copy()
+        ol_temp.iloc[:,:] = None
+        if overlap_start is not None:  # not first iteration
+            if verbose: print('Normalize by overlapping period:'+overlap_start.strftime('%Y-%m-%d'), end_d.strftime('%Y-%m-%d'))
+            #normalize using the maximum value of the overlapped period
+            y1 = temp.loc[overlap_start:end_d].iloc[:,0].values.max()
+            y2 = df.loc[overlap_start:end_d].iloc[:,-1].values.max()
+            coef = y2/y1
+            temp = temp * coef
+            ol_temp.loc[overlap_start:end_d, :] = 1 
+
+        df = pd.concat([df,temp], axis=1)
+        ol = pd.concat([ol, ol_temp], axis=1)
+        # shift the timeframe for next iteration
+        overlap_start = itr_d
+        end_d -= (delta-overlap)
+        itr_d -= (delta-overlap)
+        # in case of short query interval getting banned by server
+        time.sleep(sleep)
+    
+    df.sort_index(inplace=True)
+    ol.sort_index(inplace=True)
+    #The daily trend data is missing the most recent 3-days data, need to complete with hourly data
+    if df.index.max() < init_end_d : 
+        tf = 'now 7-d'
+        hourly = _fetch_data(trendreq, [keyword], timeframe=tf, cat=cat, geo=geo, gprop=gprop)
+        hourly.drop(columns=['isPartial'], inplace=True)
+        
+        #convert hourly data to daily data
+        daily = hourly.groupby(hourly.index.date).sum()
+        
+        #check whether the first day data is complete (i.e. has 24 hours)
+        daily['hours'] = hourly.groupby(hourly.index.date).count()
+        if daily.iloc[0].loc['hours'] != 24: daily.drop(daily.index[0], inplace=True)
+        daily.drop(columns='hours', inplace=True)
+        
+        daily.set_index(pd.DatetimeIndex(daily.index), inplace=True)
+        daily.columns = [tf]
+        
+        ol_temp = daily.copy()
+        ol_temp.iloc[:,:] = None
+        # find the overlapping date
+        intersect = df.index.intersection(daily.index)
+        if verbose: print('Normalize by overlapping period:'+(intersect.min().strftime('%Y-%m-%d'))+' '+(intersect.max().strftime('%Y-%m-%d')))
+        # scaling use the overlapped today-4 to today-7 data
+        coef = df.loc[intersect].iloc[:,0].max() / daily.loc[intersect].iloc[:,0].max()
+        daily = (daily*coef).round(decimals=0)
+        ol_temp.loc[intersect,:] = 1
+        
+        df = pd.concat([daily, df], axis=1)
+        ol = pd.concat([ol_temp, ol], axis=1)
+
+    # taking averages for overlapped period
+    df = df.mean(axis=1)
+    ol = ol.max(axis=1)
+    # merge the two dataframe (trend data and overlap flag)
+    df = pd.concat([df,ol], axis=1)
+    df.columns = [keyword,'overlap']
+    # Correct the timezone difference
+    df.index = df.index + timedelta(minutes=tz)
+    df = df[start_d:init_end_d]
+    # re-normalized to the overall maximum value to have max =100
+    df[keyword] = (100*df[keyword]/df[keyword].max()).round(decimals=0)
+    
+    return df
+
+# First create an empty dataframe to append the prefecture level data to
+trends_data_prefecture = pd.DataFrame()
+
+# Create a for loop that collects the data for each prefecture
+for x in prefectures:
+    try:
+        # Request the data from Google Trends for the nth prefecture
+        df_prefecture = get_daily_trend(pytrend, keyword, start, end, geo = str(geo) + '-' + str(prefectures[x]), cat=cat, gprop=gprop, verbose=True, tz=0)
+
+        # Drop the overlap column
+        df_prefecture = df_prefecture.drop('overlap', 1)
+
+        # Create a column with the prefecture name
+        df_prefecture['prefecture'] = x
+
+        # Append the data to the trends_data_prefecture dataframe
+        trends_data_prefecture = trends_data_prefecture.append(df_prefecture)
+
+        print(str(x)+" is complete")
+
+        # Pause for 10 seconds to stop Google from blocking requests
+        time.sleep(10)
+
+    except:
+        pass
+
+print("For loop complete")
+
+# Rename the column containing the data to the keyword + _trend
+trends_data_prefecture.columns = [str(keyword).replace('+', '_')+'_trend', 'prefecture']
+
+trends_data_prefecture.to_csv('./tseng_method/tseng_method_data/' + str(keyword).replace('+', '_') + '_trends_data_prefecture.csv')
